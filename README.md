f74422ef69b5c3afeff4059cc636788d34aefb8d
hvu <hoang040507@gmail.com>
2025-12-08 13:29:55 -0600

    Add SDL submodule

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+credit: 
+- cmake project setup: https://www.youtube.com/watch?v=Mi47TQ4Tsr8
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+   std::cout << "Hello world\n";
+}
